3d7b9970b19df00060d4df35870dff4f8834cb65
Manju KC <Manju KC>
2019-07-11 20:22:09 -0400

    Added gitignore and fixed seed

--- data/seeds/001-user.js
+++ data/seeds/001-user.js
@@ -1,16 +1,11 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex
-  .raw("TRUNCATE TABLE user RESTART IDENTITY CASCADE")
- 
-    .then(function () {
-      // Inserts seed entries
-      return knex('user').insert([
-        { username: 'admin', password:'1', email:'@example.com'},
-        { username: 'a', password:'1',email:'@example.com'},
-        {username: 'b', password:'1',email:'@example.com'}
-        
+exports.seed = function(knex, Promise) {
+  return knex("user")
+    .del()
+    .then(function() {
+      return knex("user").insert([
+        { username:'admin', password:'1', email:'raj@example.com' },
+        { username: 'a', password:'1',email:'manju@example.com' },
+        { username: 'b', password:'1',email:'blabla@example.com'}
       ]);
     });
 };
