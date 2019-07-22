e8c8f04c200814cedf7ee617973e5a2f3de92b83
ParrishJ <jarparrish@gmail.com>
2019-07-22 15:12:34 -0700

    be get and post added for dreams

---
+++ dreams/dreams-model.js
@@ -0,0 +1,24 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+    addDream,
+    getDreamById
+}
+
+function getDreamById(id) {
+    let query = db('dreams');
+
+    if (id) {
+        return query
+            .where('id', id)
+            .first()
+    }
+
+    return query;
+}
+
+function addDream(dream) {
+    return db('dreams')
+        .insert(dream, 'id')
+}
+
