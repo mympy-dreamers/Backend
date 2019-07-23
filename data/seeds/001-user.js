9a58b0abdc517d376622f6e6568d34bece35d415
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-07-23 16:08:51 -0500

    fixed truncate on user seed

--- data/seeds/001-user.js
+++ data/seeds/001-user.js
@@ -1,12 +1,18 @@
 const bcrypt = require('bcryptjs');
+
 exports.seed = function (knex, Promise) {
-  return knex('user').del()
+  const users = [
+    { username: 'admin', password: bcrypt.hashSync('123', 10), email: 'raj@example.com' },
+    { username: 'a', password: bcrypt.hashSync('123', 10), email: 'manju@example.com' },
+    { username: 'b', password: bcrypt.hashSync('123', 10), email: 'blabla@example.com' }
+  ]
+
+  return (knex
+    // Deletes ALL existing entries for users table
+    .raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
     .then(function () {
-      return knex("user").insert([
-        { username: 'admin', password: bcrypt.hashSync('1', 10), email: 'raj@example.com' },
-        { username: 'a', password: bcrypt.hashSync('1', 10), email: 'manju@example.com' },
-        { username: 'b', password: bcrypt.hashSync('1', 10), email: 'blabla@example.com' }
-      ]);
-    });
+      return knex("users").insert(users);
+    })
+  );
 };
 
