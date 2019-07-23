7742ec869942a2bc4d908b042037262ba412c28c
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-07-23 16:07:39 -0500

    changed users table name

--- data/migrations/20190711175645_create_user_table.js
+++ data/migrations/20190711175645_create_user_table.js
@@ -1,29 +1,29 @@
 
-exports.up = function(knex, Promise) {
-    return knex.schema
-      .createTable("user", tbl => {
-        tbl.increments()
-        
-          
-        tbl
-          .string("username", 128)
-          .notNullable()
-          .unique()
-          
-        tbl
-          .string("password", 128)
-          .notNullable()
+exports.up = function (knex, Promise) {
+  return knex.schema
+    .createTable("users", tbl => {
+      tbl.increments()
 
-          tbl
-          .string("email", 128)
-          .notNullable()
-          .unique()
-          
-       
-      })
-      
-  };
-  
-  exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists("user");
-  };
+
+      tbl
+        .string("username", 128)
+        .notNullable()
+        .unique()
+
+      tbl
+        .string("password", 128)
+        .notNullable()
+
+      tbl
+        .string("email", 128)
+        .notNullable()
+        .unique()
+
+
+    })
+
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists("users");
+};
