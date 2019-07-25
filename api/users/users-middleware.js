85a3184e7e3d3e9e3ed50a73de4dba5ec505e483
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-07-24 22:13:26 -0700

    validation middleware (not auth)

---
+++ api/users/users-middleware.js
@@ -0,0 +1,24 @@
+const userDB = require('../models/users-model');
+
+module.exports = {
+    validateUserId: async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const user = await userDB.get(id);
+    
+            user 
+            ? next()
+            : res.status(400).json({message: 'invalid id'});  
+            
+        } catch(err) {
+            res.status(404).json({message: 'cannot find user id'});
+        }
+    },
+    validateUserBody: (req, res, next) => {
+        const { username, password, email } = req.body;
+    
+        username && password && email
+        ? next()
+        : res.status(400).json({message: 'Missing required fields.'})
+    }   
+}
