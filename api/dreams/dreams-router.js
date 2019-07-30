6f64ae9302112d079846214f0da71bd6af76c697
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-07-30 18:40:57 -0500

    fixed conflicts in put

--- api/dreams/dreams-router.js
+++ api/dreams/dreams-router.js
@@ -7,7 +7,7 @@ module.exports = router;
 router.get('/', getDreams)
 router.get('/:id', getDreamById);
 router.post('/', validateDreamBody, addDream);
-router.put('/:id', validateDreamBody, validateDreamId, updateDream);
+router.put('/:id', validateDreamId, updateDream);
 router.delete('/:id', validateDreamId, deleteDream);
 
 
@@ -33,7 +33,7 @@ function getDreamById(req, res) {  //fetches dream by dream id
 }
 
 //I added the middleware according to what is notNullable
-function addDream(req, res) { 
+function addDream(req, res) {
 
     dreamsModel.addDream(req.body)
         .then(ids => {
@@ -45,26 +45,26 @@ function addDream(req, res) {
 }
 
 function updateDream(req, res) {
-    const {id} = req.params;
+    const { id } = req.params;
 
     dreamsModel.updateDream(id, req.body)
-    .then(dream => {
-        res.status(200).json(dream)
-    })
-    .catch(error => {
-        res.status(400).json(error)
-    })
+        .then(([dream]) => {
+            res.status(200).json(dream)
+        })
+        .catch(error => {
+            res.status(400).json(error)
+        })
 }
 
 function deleteDream(req, res) {
-    const {id} = req.params;
+    const { id } = req.params;
 
     dreamsModel.removeDream(id)
-    .then(dream => {
-        res.status(200).json(dream)
-    })
-    .catch(error => {
-        res.status(400).json(error)
-    })
+        .then(dream => {
+            res.status(200).json(dream)
+        })
+        .catch(error => {
+            res.status(400).json(error)
+        })
 }
 
