c1d52af4c9ea6b8081272a9e4e7aaec772cd9017
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-08-08 11:25:54 -0500

    changed user migration

--- data/migrations/20190711175645_create_user_table.js
+++ data/migrations/20190711175645_create_user_table.js
@@ -4,21 +4,27 @@ exports.up = function (knex, Promise) {
     .createTable("users", tbl => {
       tbl.increments()
 
+      tbl.varchar("auth_id").unique();
+
       tbl
         .string("username", 128)
         .notNullable()
         .unique()
 
       tbl
-        .string("password", 128)
-        .notNullable()
+        .string("password");
 
       tbl
         .string("email", 128)
         .notNullable()
         .unique()
+
+      tbl
+        .timestamp("created_at", { useTz: false })
+        .notNullable()
+        .defaultTo(knex.fn.now());
+
     })
-    
 };
 
 exports.down = function (knex, Promise) {
