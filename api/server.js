df74e2e2b3f7e5238415d49e003b1c056619913d
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-08-19 18:04:58 -0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const authRouter = require('./auth/signUp-router.js');
 const authZeroRouter = require('./auth/auth0-router');
 const userRouter = require('./users/users-router.js');
 const dreamsRouter = require('./dreams/dreams-router');
+const journalsRouter = require('./journals/journals-router');
 const sendEmail = require('./sendgrid/sendgrid.js');
 const imagesRouter = require('./cloudinary/cloudinary');
 
@@ -15,6 +16,7 @@ server.use(express.json());
 server.use("/auth", authRouter);
 server.use("/api/users", userRouter);
 server.use("/api/dreams", dreamsRouter);
+server.use("/api/journals", journalsRouter);
 server.use("/mail", sendEmail);
 server.use('/api/images', imagesRouter)
 server.use('/auth/zero', authZeroRouter);
