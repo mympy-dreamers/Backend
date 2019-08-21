87c4164bdef30f04377a64be5154bd45a44133fb
Krisli Dimo <krislidimo@gmail.com>
2019-08-20 23:39:34 -0500

    setup stripeRouter
    Co-authored-by: Jared-Parrish <jarparrish@gmail.com>
    Co-authored-by: Dakotah-Hitze <dakotahhitze@gmail.com>

--- api/server.js
+++ api/server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const server = express();
 
+server.use(require("body-parser").text());
+
+
 const authRouter = require('./auth/signUp-router.js');
 const authZeroRouter = require('./auth/auth0-router');
 const userRouter = require('./users/users-router.js');
 const dreamsRouter = require('./dreams/dreams-router');
 const sendEmail = require('./sendgrid/sendgrid.js');
 const imagesRouter = require('./cloudinary/cloudinary');
+const stripeRouter = require('./stripe/stripe');
 
 server.use(cors());
 server.use(express.json());
@@ -18,6 +22,7 @@ server.use("/api/dreams", dreamsRouter);
 server.use("/mail", sendEmail);
 server.use('/api/images', imagesRouter)
 server.use('/auth/zero', authZeroRouter);
+server.use('/stripe', stripeRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({ Title: 'Mympy Server Up!' });
